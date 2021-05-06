d50864f0e5285d4f8cef21f79878a3bc50074a23
Atul Kaushik <kaushikatul033@gmail.com>
2021-05-06 22:50:28 +0530

    added headline component

--- src/component/headline/index.js
+++ src/component/headline/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 class Headline extends Component {
+
+  constructor(props){
+    super(props);
+
+  }
   render() {
-    return <h1>Hello headline</h1>;
+
+    const {header,desc} = this.props;
+
+    if(!header)
+      return null;
+
+    return <div className="headlineComponent">
+        <h1 className="header">{header}</h1>
+        <p className="description">{desc}</p>
+    </div>
+    
   }
 }
 
--- src/component/headline/index.test.js
+++ src/component/headline/index.test.js
@@ -1,4 +1,51 @@
-it("should render headline", () => {
-   
-    expect(1).toBe(1);
+import React from "react";
+import { shallow } from "enzyme";
+import Headline from "./index";
+
+const setup = (props = {}) => {
+  const component = shallow(<Headline {...props} />);
+  return component;
+};
+
+describe("Headline comp", () => {
+  describe("Have props", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        header: "Test",
+        desc: "desc",
+      };
+      wrapper = setup(props);
+     
+    });
+
+    it("should render headline", () => {
+      let headline = wrapper.find('.headlineComponent')
+      expect(headline.length).toBe(1);
+    });
+
+    it("should render h1", () => {
+      let header = wrapper.find('.header')
+      expect(header.length).toBe(1);
+    });
+
+    it("should render desc", () => {
+      let desc = wrapper.find('.header')
+      expect(desc.length).toBe(1);
+    });
+
+
   });
+
+  describe("Have no  props", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup();
+    });
+    it("should not render headline", () => {
+      let headline = wrapper.find('.headlineComponent')
+      expect(headline.length).toBe(0);
+    });
+
+  });
+});
